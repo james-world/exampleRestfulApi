3e3725bfadddb298ec515aab44a3473f212dd4d7
James World <james-world@users.noreply.github.com>
2017-04-11 00:42:54 +0100

    Create project, add AspNetCore.Mvc

--- Library.API/Startup.cs
+++ Library.API/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace LearnLibrary
+namespace Library.API
 {
     public class Startup
     {
@@ -16,6 +16,7 @@ namespace LearnLibrary
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,11 +28,12 @@ namespace LearnLibrary
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler();
+            }
+
+            app.UseMvc();
         }
     }
 }
