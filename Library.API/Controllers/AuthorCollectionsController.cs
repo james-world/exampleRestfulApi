ecc1a1a730b136ed5b43b5323ea32c11c9265b06
James World <james-world@users.noreply.github.com>
2017-04-13 23:57:30 +0100

    Add AuthorCollection

---
+++ Library.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Library.API.Entities;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : Controller
+    {
+        private readonly ILibraryRepository libraryRepository;
+
+        public AuthorCollectionsController(ILibraryRepository libraryRepository)
+        {
+            this.libraryRepository = libraryRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthorCollection([FromBody]IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            if (authorCollection == null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities = Mapper.Map<IEnumerable<Author>>(authorCollection);
+
+            foreach (var authorEntity in authorEntities)
+            {
+                libraryRepository.AddAuthor(authorEntity);
+            }
+
+            if (!libraryRepository.Save())
+            {
+                throw new Exception("Creating an author collection failed on save.");
+            }
+
+            return Ok();
+        }
+    }
+}
