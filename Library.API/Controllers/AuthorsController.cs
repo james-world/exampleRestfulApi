15067a059a98827a3cb2f8636cff117259f606b7
James World <james-world@users.noreply.github.com>
2017-04-13 17:06:02 +0100

    Add GetAuthor

--- Library.API/Controllers/AuthorsController.cs
+++ Library.API/Controllers/AuthorsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using Library.API.Helpers;
@@ -25,6 +26,16 @@ namespace Library.API.Controllers
             var authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
 
             return new JsonResult(authors);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthor(Guid id)
+        {
+            var authorFromRepo = libraryRepository.GetAuthor(id);
+
+            var author = Mapper.Map<AuthorDto>(authorFromRepo);
+
+            return new JsonResult(author);
         }
     }
 }
