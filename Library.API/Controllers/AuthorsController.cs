4bd090acf6c26f67b5599ce41a4e377b5e0a4d49
James World <james-world@users.noreply.github.com>
2017-04-13 11:49:06 +0100

    Add AuthorsController

---
+++ Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,21 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly ILibraryRepository libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            this.libraryRepository = libraryRepository;
+        }
+
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
