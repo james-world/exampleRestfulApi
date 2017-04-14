9871b276332aaa0de8550286fd69b783e80caf93
James World <james-world@users.noreply.github.com>
2017-04-14 22:12:16 +0100

    Use IValidatableObject to validate differing title and description

--- Library.API/Models/BookForCreationDto.cs
+++ Library.API/Models/BookForCreationDto.cs
@@ -1,13 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Library.API.Models
 {
-    public class BookForCreationDto
+    public class BookForCreationDto : IValidatableObject
     {
         [Required(ErrorMessage = "You must provide a title.")]
         [MaxLength(100, ErrorMessage = "Title must be no more than 100 characters.")]
         public string Title { get; set; }
         [MaxLength(500, ErrorMessage = "Description must be no more than 500 characters.")]
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+                yield return new ValidationResult("Title and description must differ.", new [] {"Book"});
+        }
     }
 }
